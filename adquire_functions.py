d515f32cbaa8b40757279404489c5f04e4c79c99
albertogcmr <garcia.cobo.alberto@gmail.com>
2019-05-27 12:21:43 +0200

    refactorizando

--- adquire_functions.py
+++ adquire_functions.py
@@ -12,6 +12,7 @@ def get_students_and_katas(students_csv='input/students.csv',
                            katas_csv='input/katas.csv', 
                            students_index='username', 
                            katas_index='slug'): 
+    
     df1 = get_csv(students_csv, students_index)
     df2 = get_csv(katas_csv, katas_index)
     return df1, df2
